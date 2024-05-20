0374191dbb030213ac5844e433922c10747864cb
Ebrahim Al Aghbari <Alaghbarie@cardiff.ac.uk>
2024-05-20 21:09:18 +0100

    Handle calculator input

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    number = request.form.get('number')
+    
+    return render_template('index.html', number=number)
+
+
+def calculate():
+    pass
 
 if __name__ == '__main__':
     app.run(debug = True)
