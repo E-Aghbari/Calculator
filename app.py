a284bd5654b50a5633a2a868d12880e86f43a829
Ebrahim Al Aghbari <Alaghbarie@cardiff.ac.uk>
2024-05-22 16:57:15 +0100

    handle multi-digit numbers

--- app.py
+++ app.py
@@ -5,64 +5,77 @@ app = Flask(__name__)
 @app.route("/", methods=['GET','POST'])
 def index():
     number = request.form.get('number')
-    postfix = infixToPostfix('2 / 2')
+    postfix = infixToPostfix('(3+45)*2 /(1 - 5)')
     print(postfix)
-    print(calculate(postfix))
+    print(calculatePostfix(postfix))
     
     return render_template('index.html', number=number)
 
-def calculate(expression):
+def calculatePostfix(expression):
     stack = []
+    tokens = expression.split()
 
-    for token in expression:
-        if '0' <= token <= '9':
-            stack.append(token)
+    for token in tokens:
+        if token.isdigit():
+            stack.append(float(token))
         else:
+            operand2 = stack.pop()
+            operand1 = stack.pop()
             if token == '+':
-                stack.append(float(stack.pop(-2)) + float(stack.pop(-1)))
+                stack.append(operand1 + operand2)
             elif token == '-':
-                stack.append(float(stack.pop(-2)) - float(stack.pop(-1)))
+                stack.append(operand1 - operand2)
             elif token == '*':
-                stack.append(float(stack.pop(-2)) * float(stack.pop(-1)))
+                stack.append(operand1 * operand2)
             elif token == '/':
-                stack.append(float(stack.pop(-2)) / float(stack.pop(-1)))
+                stack.append(operand1 / operand2)
             # elif token == '^':
             #     stack.append(int(stack.pop(-2)) / int(stack.pop(-1)))
 
 
-    return stack[0]
+    return stack.pop()
 
 
-def infixToPostfix(infixEx):
+def infixToPostfix(expression):
     stack = []
-    postFix = []
-
+    postfix = []
+    current_number = ''
     precedence = {'^': 3, '/': 2, '*': 2, '+': 1, '-': 1}
+    associativity = {'^': 'R', '/': 'L', '*': 'L', '+': 'L', '-': 'L'}
 
-    for token in infixEx:
-        if  '0' <= token <= '9':
-            postFix.append(token)
-        
+    for token in expression:
+        if token.isdigit(): 
+            current_number += token
+        elif token == ' ' and current_number: 
+            postfix.append(current_number)
+            current_number = ''
+        elif token in precedence:
+            if current_number:
+                postfix.append(current_number)
+                current_number = ''
+            while (stack and stack[-1] != '(' and
+                   (precedence[stack[-1]] > precedence[token] or
+                    (precedence[stack[-1]] == precedence[token] and associativity[token] == 'L'))):
+                postfix.append(stack.pop())
+            stack.append(token)
         elif token == '(':
             stack.append(token)
-
         elif token == ')':
-            while stack and stack[-1] != '(':
-                postFix.append(stack.pop())
+            if current_number:
+                postfix.append(current_number)
+                current_number = ''
+            while stack[-1] != '(':
+                postfix.append(stack.pop())
             stack.pop()
-        
-        elif token == ' ':
-            continue
 
-        else:
-            while (stack and stack[-1] != '(' and (precedence[token] < precedence[stack[-1]] or (precedence[token] == precedence[stack[-1]] and associativity(token) == 'L'))):
-                postFix.append(stack.pop())
-            stack.append(token)
-    
+    if current_number:
+        postfix.append(current_number)
+
     while stack:
-        postFix.append(stack.pop())
+        postfix.append(stack.pop())
 
-    return (''.join(postFix))
+    return ' '.join(postfix)
+
 
 def associativity(operator):
     if operator == '^':
